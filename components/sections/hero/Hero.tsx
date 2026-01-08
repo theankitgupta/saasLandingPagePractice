9ed8b527ef02e04e0ff423696dae8b58409906d1
Ankit Gupta <theankitguptaa@gmail.com>
2026-01-08 23:07:33 +0530

    Hero Section Tablet View

--- components/sections/hero/Hero.tsx
+++ components/sections/hero/Hero.tsx
@@ -9,9 +9,9 @@ export default function Hero() {
         Essential for Hero sections to look premium and not cramped.
       - `px-12`: Adds horizontal padding.
           *TEMPLATE NOTE*: This ensures content never hits the very edge of the screen. 
-          For mobile specifically, `px-4` is often safer, but `px-12` works well for a spacious desktop look.
+          For mobile specifically, `px-4` is often safer.
     */
-    <section className="py-24 px-12">
+    <section className="py-24 px-4">
       {/* CENTERING WRAPPER
         - `container mx-auto`: Standard Tailwind pattern. 
           1. `container` sets a max-width based on the screen size (breakpoints).
@@ -35,7 +35,7 @@ export default function Hero() {
           - `text-center`: Aligns the text itself to the center.
           - `mt-6`: Spacing. A consistent "margin-top" scale (mt-6, mt-8) creates vertical rhythm.
         */}
-        <h1 className="text-6xl font-medium text-center mt-6">
+        <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
           Impactful design, created effortlessly
         </h1>
 
@@ -47,7 +47,7 @@ export default function Hero() {
           - `px-8`: Adds internal padding to this paragraph, preventing it from getting too wide visually 
             compared to the headline.
         */}
-        <p className="text-center text-xl text-white/50 mt-8 px-8">
+        <p className="text-center text-xl text-white/50 mt-8 px-2 max-w-166 mx-auto">
           Design tools shouldn&apos;t slow you down. Layers combines powerful
           features with an intuitive interface that keeps you in your creative
           flow.
@@ -61,12 +61,13 @@ export default function Hero() {
           - `border border-white/15`: Subtle boundary.
           - `rounded-full`: Fully rounded corners (Pill shape) are a modern UI trend.
         */}
-        <form className="flex justify-between border border-white/15 rounded-full p-2 mt-8">
+        <form className="flex justify-between border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
           <input
             type="email"
             placeholder="Enter your email"
             /* `bg-transparent`: Removes default white background so it blends with the dark theme. */
-            className="px-4 bg-transparent outline-none"
+            // flex-1 at medium and above screen sizes so that the input field take all the right side available empty spaces
+            className="px-4 bg-transparent outline-none md:flex-1"
           />
           <Button
             type="button"
