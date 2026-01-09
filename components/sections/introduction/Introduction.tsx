28462e7a877565c65f5dd9adaf400180fcc3436c
Ankit Gupta <theankitguptaa@gmail.com>
2026-01-09 14:08:39 +0530

    Introduction Section Desktop View

--- components/sections/introduction/Introduction.tsx
+++ components/sections/introduction/Introduction.tsx
@@ -6,12 +6,12 @@ export default function Introduction() {
     "You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.";
   return (
     // Extra vertical padding so that navbar does not cover our text while scroll animation works
-    <section className="py-28 px-4">
-      <div className="container">
+    <section className="py-28 lg:py-40 px-4">
+      <div className="container mx-auto">
         <div className="flex justify-center">
           <Tag>Introduction Layers</Tag>
         </div>
-        <div className="text-4xl md:text-6xl text-center font-medium mt-10">
+        <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
           <span>Your creative process deserves better.</span>{" "}
           <span className="text-white/15">{Intro_Text}</span>
           <span className="text-lime-400 block md:px-4">That&apos;s why we built Layers</span>
